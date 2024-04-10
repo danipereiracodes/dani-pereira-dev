0b5062a4ed48f211bb026bebf6293723d66c3dcf
danipereiracodes <danipg.dev@gmail.com>
2024-04-10 11:33:52 +0200

    add tailwind config for dark theme and add dark bg colors and dummy switcher for develop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				'primary-color': '#000',
-				'secondary-color': '#0072ab'
+				'secondary-color': '#0072ab',
+				'dark-background': 'rgb(15 23 42 / 1)',
+				'dark-secondary': 'rgba(6, 80, 116, .3)',
+				'dark-primary': 'rgb(2 6 23 / 1 )'
 			}
 		}
 	},
